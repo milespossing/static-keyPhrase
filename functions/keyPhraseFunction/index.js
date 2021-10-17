b7dba3255c22c96da8b3a21f557071f618f0b8dd
Miles Possing <7330275+milespossing@users.noreply.github.com>
2021-10-17 11:31:21 -0700

    Finishing fcns

--- functions/keyPhraseFunction/index.js
+++ functions/keyPhraseFunction/index.js
@@ -1,32 +1,22 @@
 const { TextAnalyticsClient, AzureKeyCredential } = require("@azure/ai-text-analytics");
 
 async function keyPhraseExtraction(client, input){
-    const key = process.env.cogCred;
-    const endpoint = process.env.cogEnd;
-    const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
     const keyPhrasesInput = [ input ];
     const keyPhraseResult = await client.extractKeyPhrases(keyPhrasesInput);
     
-    keyPhraseResult.forEach(document => {
-        console.log(`ID: ${document.id}`);
-        console.log(`\tDocument Key Phrases: ${document.keyPhrases}`);
-    });
+    return keyPhraseResult[0];
 }
 
 module.exports = async function (context, req) {
+    const key = process.env.cogCred;
+    const endpoint = process.env.cogEnd;
+    const textAnalyticsClient = new TextAnalyticsClient(endpoint,  new AzureKeyCredential(key));
 
-    
-
-
-
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const result = await keyPhraseExtraction(textAnalyticsClient, req.body);
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        body: result,
     };
 }
